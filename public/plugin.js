42834410e71494413b5833a5f656cd8faeaa09bb
Chijioke <osadebec98@gmail.com>
2024-12-09 09:42:26 +0100

    Auto-commit: Mon Dec  9 09:42:25 AM WAT 2024

--- public/plugin.js
+++ public/plugin.js
@@ -1,4 +1,3 @@
-/* eslint-disable @typescript-eslint/no-unused-vars */
 penpot.ui.open("Penpot Pomodoro", `?theme=${penpot.theme},`, {
   width: 320,
   height: 630,
--- src/plugin.ts
+++
@@ -1,5 +0,0 @@
-/* eslint-disable @typescript-eslint/no-unused-vars */
-penpot.ui.open(" to Replace", `?theme=${penpot.theme},`, {
-  width: 320,
-  height: 630,
-});
