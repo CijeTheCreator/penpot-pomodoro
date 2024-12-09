47c0dd4c407a94cb2b790859dcd75d8b74f76cd5
Chijioke <osadebec98@gmail.com>
2024-12-09 09:26:13 +0100

    Auto-commit: Mon Dec  9 09:26:13 AM WAT 2024

--- src/plugin.ts
+++ src/plugin.ts
@@ -1,6 +1,5 @@
 /* eslint-disable @typescript-eslint/no-unused-vars */
 import { Board, Group, ImageData, Shape } from "@penpot/plugin-types";
-import type { PluginMessageEvent } from "./model";
 
 penpot.ui.open("Paste to Replace", `?theme=${penpot.theme},`, {
   width: 320,
